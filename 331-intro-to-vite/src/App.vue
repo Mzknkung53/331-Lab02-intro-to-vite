0b835e624fa7fb523834ca501934bfdf4bb87ff5
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-07-03 22:46:13 +0700

    Question 16.3

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -9,7 +9,8 @@ import EventCard from './components/EventCard.vue'
       <div class="wrapper">
         <nav>
           <RouterLink to="/">Event</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/about">About</RouterLink> |
+          <RouterLink to="/student">Students</RouterLink>
         </nav>
       </div>
     </header>
