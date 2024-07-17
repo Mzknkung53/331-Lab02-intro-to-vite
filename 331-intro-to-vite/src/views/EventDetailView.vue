acb1dccca02ca5a04ad5d1d5fbb66d47a040f8ea
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-07-17 20:10:50 +0700

    Lab3 Q1-Q3

---
+++ 331-intro-to-vite/src/views/EventDetailView.vue
@@ -0,0 +1,22 @@
+<script setup lang ="ts">
+import {ref} from 'vue'
+import type { Ref } from 'vue'
+import type {EventItem} from '@/type'
+import EventService from '@/services/EventService'
+const event = ref <EventItem | null> (null)
+const id: Ref<number> = ref(123)
+
+    EventService.getEventById(id.value)
+    .then((response) => {
+        event.value = response.data
+    }).catch(error => {
+        console.log(error)
+    })
+</script>
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
