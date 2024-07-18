d2aeea396957fe7735ae0e0eeed0416e53795d2a
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-07-18 19:08:52 +0700

    Q2.9

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -1,14 +1,15 @@
-//import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import { createAppRouter } from './router'
 
 const app = createApp(App)
+const pageLimit = [null, 1, 1, 2, 2] 
 
 app.use(createPinia())
-app.use(router)
+app.use(createAppRouter(pageLimit))
 
 app.mount('#app')
