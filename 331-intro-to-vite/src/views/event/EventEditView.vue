cef0e53b05bc792150e59fb41a54c293ba4176dd
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-07-24 21:27:37 +0700

    Lab4 Q1.6

--- 331-intro-to-vite/src/views/event/EventEditView.vue
+++ 331-intro-to-vite/src/views/event/EventEditView.vue
@@ -1,13 +1,26 @@
-<script setup lang ="ts">
-import type {Event} from '@/type'
-import type { PropType } from 'vue'
-defineProps({
-    event: {
-        type: Object as PropType<Event>,
-            require: true
-    }
-})
+<script setup lang="ts">
+import type { Event } from '@/type'
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+
+const props = defineProps<{
+  event: Event
+}>()
+
+const { event } = toRefs(props)
+const router = useRouter()
+const store = useMessageStore()
+const edit = () => {
+
+  store.updateMessage('Event ' + props.event.title + 'has been update.')
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
+}
 </script>
+
 <template>
-    <p>Edit the event here</p>
+  <button @click="edit">Edit</button>
 </template>
