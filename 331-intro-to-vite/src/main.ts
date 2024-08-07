71e366a63fcbf3a54a17e3ed50d07a18b29c7826
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-08-07 22:21:17 +0700

    Q13.1-13.7

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -7,10 +7,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import 'nprogress/nprogress.css'
 import { createAppRouter } from './router'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 const pageLimit = [null, 1, 1, 2, 2] 
 
+inject()
 app.use(createPinia())
 app.use(createAppRouter(pageLimit))
 
